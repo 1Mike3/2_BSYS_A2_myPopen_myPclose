c218ed2f630e3215f9faf80ea43a4bf24528f276
michi <michalkuc55@gmail.com>
2023-05-20 23:41:22 +0200

    formated nicely

--- main.c
+++ main.c
@@ -5,41 +5,44 @@
 
 
 int main() {
-    FILE * ls = mypopen("ls -l","r");
-    if (ls == NULL){
+
+    printf("START MYPOPEN\n");
+
+    FILE * stream1 = mypopen("ls -l", "r");
+    if (stream1 == NULL){
         printf("ERROR 1");
         return 1;
     }
 
 
 
-  //  printPipe(ls);
+  //  printPipe(stream1);
  //   printf("TEST\n");
 
-    FILE * wc = mypopen("wc","w");
+    FILE * stream2 = mypopen("wc", "w");
 
 
 
 
-// we consume the output of `ls` and feed it to `wc`
+// feeding stream1 to stream2
     char buf[1024];
-    while (fgets(buf, sizeof(buf), ls) != NULL)
-        fputs(buf, wc);
+    while (fgets(buf, sizeof(buf), stream1) != NULL)
+        fputs(buf, stream2);
 // once
-   // printf("--stuff has just been witten to wc!\n");
+   // printf("--stuff has just been witten to stream2!\n");
 
 
 
 
-if (wc == NULL){
+if (stream2 == NULL){
     printf("ERROR 2");
     return 1;
 }
 
 // sleep(5);
 
-    mypclose(ls);
-    mypclose(wc);
+    mypclose(stream1);
+    mypclose(stream2);
 
 
 
